354ea0aedfb0e2ceb5cfc277ff1fe29cdbb638e1
Aryan Niknami <niknamiaryan@gmail.com>
2021-08-04 02:55:47 +0430

    modified parser file for using generator

--- large_file_parser.py
+++ large_file_parser.py
@@ -2,26 +2,42 @@
 import os
 import time
 
+
 start = time.time()
 
-file_name = "data.txt"
-
+file_name = "data-files\examples\large_data.txt"
 print(f'File Size is {os.stat(file_name).st_size / (1024 * 1024)} MB')
 
-txt_file = open(file_name,'rb')
 
-count = 0
+def split_by_delimiter(rows, delimiter):
+    for row in rows:
+        yield str(row).split(delimiter)
 
-for line in txt_file:
-    # we can process file line by line here, for simplicity I am taking count of lines
-    count += 1
 
-txt_file.close()
+def read_file(fp):
+    while True:
+        line = fp.readline()
+        if line is None:
+            break
+        yield line
 
-print(f'Number of Lines in the file is {count}')
 
-end = time.time()
-print("Execution time in seconds: ",(end - start))
+
+
+if __name__ == '__main__':
+    with open(file_name, "rb") as fp:
+        lines = read_file(fp)
+        lines = split_by_delimiter(lines, "|")
+
+        for line in lines:
+            print(line)
+            
+    end = time.time()
+    print("Execution time in seconds: ",(end - start))
+
+
+
+
 
 
 
@@ -30,13 +46,3 @@ print("Execution time in seconds: ",(end - start))
 # print('Peak Memory Usage =', resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
 # print('User Mode Time =', resource.getrusage(resource.RUSAGE_SELF).ru_utime)
 # print('System Mode Time =', resource.getrusage(resource.RUSAGE_SELF).ru_stime)
-
-
-
-
-# with open(file_name) as f:
-#     while True:
-#         data = f.read(1024)
-#         if not data:
-#             break
-#         print(data)
